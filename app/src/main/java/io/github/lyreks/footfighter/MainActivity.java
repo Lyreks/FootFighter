4015c18df43d463f97b7ed3056a27ce99b44d322
grahamreese <grahamreese.95@gmail.com>
2018-06-28 22:00:33 -0400

    initial implementation of pedometer

--- app/src/main/java/io/github/lyreks/footfighter/MainActivity.java
+++ app/src/main/java/io/github/lyreks/footfighter/MainActivity.java
@@ -1,13 +1,56 @@
 package io.github.lyreks.footfighter;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    /* reeeeeeeee */
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView tv_steps;
+    SensorManager sensorManager;
+    boolean running = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_steps = findViewById(R.id.tv_steps);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    protected void onResume(){
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor != null){
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_GAME);
+        } else{
+            Toast.makeText(this, "Sensor not found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onPause(){
+        super.onPause();
+        running = false;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(running){
+            tv_steps.setText(String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
